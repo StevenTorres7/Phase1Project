a27a153da37ad832048bccc4466c1fbda92b176b
StevenTorres7 <57144016+StevenTorres7@users.noreply.github.com>
2023-01-18 02:20:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let sneakers = {
+    brand: "Nike",
+    color: "black", 
+    month: "january",
+
+}
+
+// , "febuary","march","april","may","june","july","april","may","june","july","august","september","october","november","december",
